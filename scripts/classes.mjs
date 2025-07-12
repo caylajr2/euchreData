a7065bbadae6c263c266b2460abdf6ac8c82f119
caylajr2 <73130112+caylajr2@users.noreply.github.com>
2025-07-11 20:32:08 -0500

    create initial classes - Card, Bid, Trick, Hand, Game with basic test functions for Card and Trick

---
+++ scripts/classes.mjs
@@ -0,0 +1,254 @@
+const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
+const values = ['9', '10', 'J', 'Q', 'K', 'A'];
+
+
+class Card {
+    constructor(suit, value) {
+        if (!suits.includes(suit)) {
+            throw Error('Suit must be one of the following: spades, hearts, diamonds, clubs');
+        }
+        if (!values.includes(value)) {
+            throw Error('Value must be one of the following: 9, 10, J, Q, K, A')
+        }
+        this._suit = suit;
+        this._value = value;
+    }
+    get suit() {
+        return this._suit;
+    }
+    get value() {
+        return this._value;
+    }
+}
+
+class Bid {
+    constructor(name, suit, tricks) {
+        if (!suits.includes(suit)) {
+            throw Error('Suit must be one of the following: spades, hearts, diamonds, clubs');
+        }
+        this._name = name;
+        this._suit = suit;
+        this._tricks = tricks;
+    }
+    get suit() {
+        return this._suit;
+    }
+    get tricks() {
+        return this._tricks
+    }
+}
+
+class Trick {
+    constructor() {
+        this._cards = [];
+    }
+    get cards() {
+        return this._cards;
+    }
+    playCard(card) {
+        if (this._cards.length >= 6) {
+            throw Error('There cannot be more than 6 cards per trick.');
+        }
+        if (!(card instanceof Card)) {
+            throw Error('You can only play objects of type Card on tricks.');
+        }
+        this._cards.push(card);
+    }
+    removeCard() {
+        if (this._cards.length === 0) {
+            throw Error('There must be at least 1 card to remove.');
+        }
+        this._cards.pop();
+    }
+    toString() {
+        let str = "{\n";
+        for (const card of this._cards) {
+            str += `\t ${card.suit} ${card.value}\n`;
+        }
+        str += '}';
+        return str;
+    }
+}
+
+class Hand {
+    constructor() {
+        this._bids = [];
+        this._tricks = [];
+    }
+    get bids() {
+        return this._bids;
+    }
+    get tricks() {
+        return this._tricks;
+    }
+    addBid(bid) {
+        if (this._bids.length >= 6) {
+            throw Error('There cannot be more than 6 bids.');
+        }
+        if (!(bid instanceof Bid)) {
+            throw Error('You can only add objects of type Bid here.');
+        }
+        this._bids.push(bid);
+    }
+    removeBid() {
+        if (this._bids.length === 0) {
+            throw Error('There must be at least 1 bid to remove.');
+        }
+        this._bids.pop();
+    }
+    addTrick(trick) {
+        if (this._tricks.length >= 8) {
+            throw Error('There cannot be more than 8 tricks.');
+        }
+        if (!(trick instanceof Trick)) {
+            throw Error('You can only add objects of type Trick here.');
+        }
+        this._tricks.push(trick);
+    }
+    removeTrick() {
+        if (this._tricks.length === 0) {
+            throw Error('There must be at least 1 trick to remove.');
+        }
+        this._tricks.pop();
+    }
+}
+
+class Game {
+    constructor() {
+        this._hands = [];
+    }
+    get hands() {
+        return this._hands;
+    }
+    addHand(hand) {
+        if (!(hand instanceof Hand)) {
+            throw Error('You can only add objects of type Hand to the game.');
+        }
+        this._hands.push(hand);
+    }
+    removeHand() {
+        if (this._hands.length === 0) {
+            throw Error('There must be at least 1 card to remove.');
+        }
+        this._hands.pop();
+    }
+}
+
+export { Card, Bid, Trick, Hand, Game };
+
+
+
+
+    
+    
+
+    
+// =============================== Testing Functions =========================================
+
+const createDeck = () => {
+    const deck = [];
+
+    for (const suit of suits) {
+        for (const value of values) {
+            deck.push(new Card(suit, value));
+        }
+    }
+
+    //verify creation of deck and cards
+    console.log(deck);
+    return deck;
+}
+
+const removeCardFromEmptyTrick = (deck) => {
+    const trick = new Trick();
+
+    // check removing card when trick is empty
+    try {
+        trick.removeCard();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const addCardsToTrick = (deck) => {
+    const trick = new Trick();
+
+    for (let i = 0; i < 6; i++) {
+        // get random card in deck
+        let x = Math.floor(Math.random() * deck.length);
+        
+        // add card to trick
+        console.log(`Adding card: ${deck[x].suit} ${deck[x].value}`);
+        trick.playCard(deck[x]);
+
+        // print out to verify
+        console.log(trick.toString());
+    }
+}
+
+const removeCardFromTrick = (deck) => {
+    const trick = new Trick();
+    let numCards = Math.floor(Math.random() * 5)+1;
+    for (let i = 0; i < numCards; i++) {
+        let x = Math.floor(Math.random() * deck.length);
+            
+        // add card to trick
+        trick.playCard(deck[x]);
+    }
+    console.log(`Initial Trick: ${trick.toString()}`);
+    trick.removeCard();
+    console.log(`Final Trick: ${trick.toString()}`);
+}
+
+const addCardToFullTrick = (deck) => {
+    const trick = new Trick();
+
+    for (let i = 0; i < 6; i++) {
+        // get random card in deck
+        let x = Math.floor(Math.random() * deck.length);
+        
+        // add card to trick
+        trick.playCard(deck[x]);
+
+    }
+    try {
+        // get random card in deck
+        let x = Math.floor(Math.random() * deck.length);
+        
+        // add card to trick
+        trick.playCard(deck[x]);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+    
+    
+    
+    
+
+console.log("Testing...");
+
+// Creating Cards
+console.log("Creating cards in deck:\n");
+const deck = createDeck();
+
+
+
+// Testing trick functions
+
+console.log('\n\n\nAdding 6 cards to new trick object:\n');
+addCardsToTrick(deck);
+
+console.log('\n\n\nAdding 7th card to full trick object:\n');
+addCardToFullTrick(deck);
+
+console.log('\n\n\nRemoving card from trick object:\n');
+removeCardFromTrick(deck);
+
+console.log('\n\n\nRemoving card from empty trick object:\n');
+removeCardFromEmptyTrick(deck);
+
+
+
+
