54ba23227eaa508090805364cc90ed47996483ce
Kristin <krisinger1@gmail.com>
2025-09-21 09:34:44 -0500

    trick logger styling

--- euchre-data-entry-react/src/components/Trick.jsx
+++ euchre-data-entry-react/src/components/Trick.jsx
@@ -2,12 +2,12 @@ import Card from './Card'
 import '../styles/trickStyle.css'
 import '../styles/cardImageStyle.css'
 
-const Trick = ({ cardClass, cardsPlayed }) => {
+const Trick = ({ trickClass, cardClass, cardsPlayed }) => {
     
     return (
         <>
             {/* displays cards in current trick using prop cardsPlayed */}
-            <ul className='trickRow'>
+            <ul className={`trickRow ${trickClass}`}>
                 {cardsPlayed.map(e => 
                 <li className="playedCard">
                     <Card cardClass={cardClass} card={{suit:e.suit,value: e.value}}/>
